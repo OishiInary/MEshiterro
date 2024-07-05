282cbb90870f51501c519b2fc3885d90759e5c70
OishiInary <168375833+OishiInary@users.noreply.github.com>
2024-07-05 04:15:36 +0000

    7/5 13:15

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,14 @@
 class PostImage < ApplicationRecord
+  
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites,     dependent: :destroy
   has_one_attached :image
+  
+  validates :shop_name, presence: true
+  validates :image, presence: true
+  
+  
   
   def get_image
     unless image.attached?
@@ -9,4 +17,8 @@ class PostImage < ApplicationRecord
     end  
     image
   end  
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
